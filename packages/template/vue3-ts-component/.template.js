e7172268da3561bbfdaad96f082942cf307a9dfe
刘磊 <liulei@bilibili.com>
2021-10-11 19:12:22 +0800

    feat: add upgrade command & upgrade script

--- packages/template/vue3-ts-component/.template.js
+++ packages/template/vue3-ts-component/.template.js
@@ -1,7 +1,20 @@
-const generateCurrentTime = async () => {
+const init = async (opts) => {
+  console.log('this hook called before check version', opts);
+}
+
+const copy = async (opts) => {
+  console.log('this hook called before copy files', opts);
   return {
     currentTime: Date.now()
   }
 }
 
-module.exports = generateCurrentTime
+const done = async (opts) => {
+  console.log('this hook called after copy files', opts);
+}
+
+module.exports = {
+  init,
+  copy,
+  done
+}
