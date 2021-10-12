2201cac261f1b33c03374dfb367df97ac45ae389
falstack <icesilt@outlook.com>
2021-10-12 08:44:46 +0800

    feat: update dapp frontend plugin

--- packages/template/hardhat-vue3-dapp/vite.config.ts
+++ packages/template/hardhat-vue3-dapp/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import host from 'vite-plugin-host'
 import strip from '@rollup/plugin-strip'
-import components from 'vite-plugin-components'
+import components from 'unplugin-vue-components/vite'
 import styleImport from 'vite-plugin-style-import'
 
 const isProd = process.env.NODE_ENV === 'production'
